c92d43d02835518fa14689b001bc22e58c24e3e4
ladorayn <lado.rayhan@ui.ac.id>
2023-03-28 14:28:29 +0700

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Lado Rayhan on 28/03/23.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
